6e6a077d358a55de304fc8dc81d853c16cb820ff
connecthub-cyber <afeez.adebola93@gmail.com>
2025-09-03 12:30:12 -0400

    Re-added output: export and triggered new deployment

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import("next").NextConfig} */
-/** @type {import("next").NextConfig} */
 const nextConfig = {
-
   output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;build
+module.exports = nextConfig;
+
 
